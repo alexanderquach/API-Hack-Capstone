6eed0b48621915338a6a95ffb35fbf6241557f29
Alexander Quach <alexquac@gmail.com>
2018-12-12 23:02:47 -0800

    updated index.html and added script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function formatQueryParams(params) {
+  //Formats query parameters for API request
+};
+
+function searchNationalParks(query) {
+  //Requests from NPS API for parks by state
+};
+
+function displayResults(responseJsonNPS) {
+  //Displays results of the response from NPS API
+};
+
+function handleUserSubmit() {
+  //Handles user submit for search
+};
+
+$(function() {
+  console.log('App loaded, awaiting user submit');
+  handleUserSubmit();
+});
